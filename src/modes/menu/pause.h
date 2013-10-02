c8c405771e808cc0ac9e6b540034fef4d78a3226
Jacob Hegna <jacobhegna@gmail.com>
2013-10-01 20:04:03 -0500

    Added a better pause menu

--- src/modes/menu/pause.h
+++ src/modes/menu/pause.h
@@ -1,10 +1,17 @@
 #ifndef _PAUSE_H
 #define _PAUSE_H
 
+#include <string>
+
 #include "../engine/engine.h"
 
 namespace PauseMenu {
 	extern bool pauseBuf;
+
+	extern std::string options[];
+	extern int         select;
+	extern bool        wBuf;
+	extern bool        sBuf;
 
 	void init(Mode::Engine*);
 	void logic(Mode::Engine*);
---
+++ src/utilities/array_size.h
@@ -0,0 +1,7 @@
+#ifndef _ARRAY_SIZE_H
+#define _ARRAY_SIZE_H
+
+// I don't know where else to put this macro... sorry
+#define ARRAY_SIZE(a) sizeof(a)/sizeof(a[0])
+
+#endif
